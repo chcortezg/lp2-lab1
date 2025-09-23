b2922b346c64bda4089d2c9001a446906111bdb3
chcortezg <chcortezgomes@gmail.com>
2025-09-23 16:43:16 -0300

    construtor adiconado em Item.java

--- lab1/src/lab1/restaurante/Item.java
+++ lab1/src/lab1/restaurante/Item.java
@@ -5,20 +5,26 @@ public class Item {
     // atributos
     private String nomeItem;
     private double precoItem;
+
+    // construtor
+    public Item(String nomeItem, double precoItem) {
+        this.nomeItem = nomeItem;
+        this.precoItem = precoItem;
+    }
     
     // getters e setters
 	public String getNomeItem() {
 		return nomeItem;
 	}
-	
+
 	public void setNomeItem(String nomeItem) {
 		this.nomeItem = nomeItem;
 	}
-	
+
 	public double getPrecoItem() {
 		return precoItem;
 	}
-	
+
 	public void setPrecoItem(double precoItem) {
 		this.precoItem = precoItem;
 	}
