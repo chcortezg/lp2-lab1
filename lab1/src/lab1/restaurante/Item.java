d8c246b803ba90120240cf0531d72b21edaae8dd
chcortezg <chcortezgomes@gmail.com>
2025-09-23 16:33:21 -0300

    implementação de Item.java

---
+++ lab1/src/lab1/restaurante/Item.java
@@ -0,0 +1,26 @@
+package lab1.restaurante;
+
+public class Item {
+
+    // atributos
+    private String nomeItem;
+    private double precoItem;
+    
+    // getters e setters
+	public String getNomeItem() {
+		return nomeItem;
+	}
+	
+	public void setNomeItem(String nomeItem) {
+		this.nomeItem = nomeItem;
+	}
+	
+	public double getPrecoItem() {
+		return precoItem;
+	}
+	
+	public void setPrecoItem(double precoItem) {
+		this.precoItem = precoItem;
+	}
+
+}
